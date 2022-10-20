98fe8244bbc1a719d7f194e8c22b3d10d311b843
Dwiko Indrawansyah <wesibrani@gmail.com>
2022-10-20 14:04:10 +0700

    Menampilkan Data dari API ke Peta

--- app/src/main/java/org/d3if4097/mobpro2/MapsActivity.kt
+++ app/src/main/java/org/d3if4097/mobpro2/MapsActivity.kt
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import org.d3if4097.mobpro2.model.Provinsi
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var map: GoogleMap
+
     private val viewModel: MapsViewModel by lazy {
         ViewModelProvider(this).get(MapsViewModel::class.java)
     }
@@ -20,9 +26,27 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        viewModel.getData().observe(this, { data ->
+            data.forEach { createMarker(it) }
+        })
     }
 
     override fun onMapReady(gMap: GoogleMap) {
+        map = gMap
         viewModel.requestData()
+
+        val lokasi = LatLng(-6.920432082789247, 107.60370834146391)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(lokasi, 7f))
+    }
+
+    private fun createMarker(prov: Provinsi) {
+        val lokasi = LatLng(prov.lokasi.lat, prov.lokasi.lon)
+        val snippet = getString(R.string.jumlah_dirawat, prov.dirawat)
+        map.addMarker(
+            MarkerOptions()
+            .position(lokasi)
+            .title(prov.key)
+            .snippet(snippet))
     }
 }
--- app/src/main/java/org/d3if4097/mobpro2/MapsViewModel.kt
+++ app/src/main/java/org/d3if4097/mobpro2/MapsViewModel.kt
@@ -1,21 +1,28 @@
 package org.d3if4097.mobpro2
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.d3if4097.mobpro2.model.Provinsi
 
 class MapsViewModel : ViewModel() {
+    private val data = MutableLiveData<List<Provinsi>>()
+
     fun requestData() = viewModelScope.launch(Dispatchers.IO) {
         withContext(Dispatchers.IO) {
             try {
                 val result = Covid19Api.service.getProvinsi()
-                Log.d("MAPS", "Jumlah data: " + result.data.size)
+                data.postValue(result.data)
             } catch (e: Exception) {
                 Log.d("MAPS", e.message.toString())
             }
         }
     }
+
+    fun getData(): LiveData<List<Provinsi>> = data
 }
