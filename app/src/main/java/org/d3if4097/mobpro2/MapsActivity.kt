80bdcc6cda1f12cbff326b868044a7ca7537e4f4
Dwiko Indrawansyah <wesibrani@gmail.com>
2022-10-20 14:08:46 +0700

    Menambahkan Map Type

--- app/src/main/java/org/d3if4097/mobpro2/MapsActivity.kt
+++ app/src/main/java/org/d3if4097/mobpro2/MapsActivity.kt
@@ -3,6 +3,8 @@ package org.d3if4097.mobpro2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -49,4 +51,29 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .title(prov.key)
             .snippet(snippet))
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.map_options, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.normal_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
+            true
+        }
+        R.id.hybrid_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_HYBRID
+            true
+        }
+        R.id.satellite_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            true
+        }
+        R.id.terrain_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+
 }
