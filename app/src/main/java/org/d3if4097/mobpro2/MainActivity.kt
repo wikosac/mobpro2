d5c6466e6537eea3ba195ce5080b04b0a47b7c53
Dwiko Indrawansyah <wesibrani@gmail.com>
2022-10-27 10:57:37 +0700

    up agp

--- app/src/main/java/org/d3if4097/mobpro2/MainActivity.kt
+++ app/src/main/java/org/d3if4097/mobpro2/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
