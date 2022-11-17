49f67d0f20083dad9df0af8cdabb6edfa7d73338
Dwiko Indrawansyah <wesibrani@gmail.com>
2022-11-17 14:01:39 +0700

    Automated Testing untuk Action Mode

--- app/src/androidTest/java/org/d3if4097/mobpro2/MainActivityTest.kt
+++ app/src/androidTest/java/org/d3if4097/mobpro2/MainActivityTest.kt
@@ -1,14 +1,21 @@
 package org.d3if4097.mobpro2
 
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.ViewInteraction
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import org.junit.Assert.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import org.d3if4097.mobpro2.data.Mahasiswa
 import org.d3if4097.mobpro2.data.MahasiswaDb
 import org.junit.Before
@@ -48,5 +55,43 @@ class MainActivityTest {
         onView(withText(MAHASISWA_DUMMY.nama)).check(matches(isDisplayed()))
 // Tes selesai, tutup activity nya
         activityScenario.close()
+    }
+
+    @Test
+    fun testActionMode() {
+// Masukkan beberapa mahasiswa sebagai data awal
+        runBlocking(Dispatchers.IO) {
+            val dao = MahasiswaDb.getInstance(getApplicationContext()).dao
+            dao.insertData(MAHASISWA_DUMMY)
+            dao.insertData(MAHASISWA_DUMMY)
+            dao.insertData(MAHASISWA_DUMMY)
+        }
+// Jalankan MainActivity
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+// Lakukan long click terhadap data pertama di RecyclerView,
+// lalu cek apakah action mode nya muncul / menu delete tampil
+        onView(withId(R.id.recyclerView)).atItem(0, longClick())
+        onView(withId(R.id.menu_delete)).check(matches(isDisplayed()))
+// Lakukan klik terhadap data ke-3 dan ke-2 pada RecyclerView,
+// lalu cek apakah action mode menampilkan jumlah data terpilih
+        onView(withId(R.id.recyclerView)).atItem(2, click())
+        onView(withId(R.id.recyclerView)).atItem(1, click())
+        onView(withText("3")).check(matches(isDisplayed()))
+// Lakukan klik terhadap menu hapus, lalu konfirmasi hapus
+// Cek apakah data mahasiswanya sudah tidak ada lagi
+        onView(withId(R.id.menu_delete)).perform(click())
+        onView(withText(R.string.hapus)).perform(click())
+        onView(withText(MAHASISWA_DUMMY.nim)).check(doesNotExist())
+        onView(withText(MAHASISWA_DUMMY.nama)).check(doesNotExist())
+// Tes selesai, tutup activity nya
+        activityScenario.close()
+    }
+
+    private fun ViewInteraction.atItem(pos: Int, action: ViewAction) {
+        perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                pos, action
+            )
+        )
     }
 }
