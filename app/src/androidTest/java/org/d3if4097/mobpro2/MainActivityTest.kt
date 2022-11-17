c79e4b937518a8fd98330e62d0a768b5c9d44883
Dwiko Indrawansyah <wesibrani@gmail.com>
2022-11-17 13:37:22 +0700

    Membuat Pengujian Otomatis untuk Insert Data

---
+++ app/src/androidTest/java/org/d3if4097/mobpro2/MainActivityTest.kt
@@ -0,0 +1,41 @@
+package org.d3if4097.mobpro2
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import org.junit.Assert.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.d3if4097.mobpro2.data.Mahasiswa
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    companion object {
+        private val MAHASISWA_DUMMY = Mahasiswa(
+            0, "6706180001", "Tika Aulia Utami"
+        )
+    }
+    @Test
+    fun testInsert() {
+// Jalankan MainActivity
+        val activityScenario = ActivityScenario.launch(
+            MainActivity::class.java
+        )
+// Lakukan aksi menambah data baru
+        onView(withId(R.id.fab)).perform(click())
+        onView(withId(R.id.nimEditText)).perform(
+            typeText(MAHASISWA_DUMMY.nim))
+        onView(withId(R.id.namaEditText)).perform(
+            typeText(MAHASISWA_DUMMY.nama))
+        onView(withText(R.string.simpan)).perform(click())
+// Cek apakah hasil sesuai yang diharapkan
+        onView(withText(MAHASISWA_DUMMY.nim)).check(matches(isDisplayed()))
+        onView(withText(MAHASISWA_DUMMY.nama)).check(matches(isDisplayed()))
+// Tes selesai, tutup activity nya
+        activityScenario.close()
+    }
+}
