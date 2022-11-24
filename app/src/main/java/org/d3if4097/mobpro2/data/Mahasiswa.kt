c67e744548d5a0bba7ce2adaed33f86371ac8446
Dwiko Indrawansyah <wesibrani@gmail.com>
2022-11-24 13:14:00 +0700

    Menyimpan Data Mahasiswa ke Firebase

--- app/src/main/java/org/d3if4097/mobpro2/data/Mahasiswa.kt
+++ app/src/main/java/org/d3if4097/mobpro2/data/Mahasiswa.kt
@@ -1,6 +1,9 @@
 package org.d3if4097.mobpro2.data
 
+import com.google.firebase.database.Exclude
+
 data class Mahasiswa(
+    @get:Exclude
     val id: Int = 0,
     val nim: String,
     val nama: String
--- app/src/main/java/org/d3if4097/mobpro2/data/MahasiswaDb.kt
+++ app/src/main/java/org/d3if4097/mobpro2/data/MahasiswaDb.kt
@@ -2,10 +2,15 @@ package org.d3if4097.mobpro2.data
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.database.FirebaseDatabase
 
 class MahasiswaDb private constructor() {
+
+    private val database = FirebaseDatabase.getInstance().getReference(PATH)
+
     val dao = object : MahasiswaDao {
         override fun insertData(mahasiswa: Mahasiswa) {
+            database.push().setValue(mahasiswa)
         }
         override fun getData(): LiveData<List<Mahasiswa>> {
             val data = MutableLiveData<List<Mahasiswa>>()
@@ -16,6 +21,7 @@ class MahasiswaDb private constructor() {
         }
     }
     companion object {
+        private const val PATH = "mahasiswa"
         @Volatile
         private var INSTANCE: MahasiswaDb? = null
         fun getInstance(): MahasiswaDb {
