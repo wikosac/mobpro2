853f100b1dc335f304d2cb0b62960b716f2825da
Dwiko Indrawansyah <wesibrani@gmail.com>
2022-12-01 15:48:30 +0700

    Membuat Fragment Kelas

---
+++ app/src/main/java/org/d3if4097/mobpro2/ui/master/KelasFragment.kt
@@ -0,0 +1,28 @@
+package org.d3if4097.mobpro2.ui.master
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import org.d3if4097.mobpro2.R
+import org.d3if4097.mobpro2.databinding.FragmentKelasBinding
+
+class KelasFragment : Fragment() {
+    private lateinit var binding: FragmentKelasBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        binding = FragmentKelasBinding.inflate(layoutInflater)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val kelas = resources.getStringArray(R.array.kelas)
+        binding.listView.adapter = ArrayAdapter(requireContext(),
+            android.R.layout.simple_list_item_1, android.R.id.text1, kelas)
+        binding.listView.setOnItemClickListener { _, _, pos, _ ->
+            Log.d("KELAS", kelas[pos])
+        }
+    }
+}
