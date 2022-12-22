7da27863053d57a8fb26dc64d43a922d6c391a57
Dwiko Indrawansyah <wesibrani@gmail.com>
2022-12-22 11:55:36 +0700

    Perpindahan Komponen

--- app/src/main/java/org/d3if4097/mobpro2/MainActivity.kt
+++ app/src/main/java/org/d3if4097/mobpro2/MainActivity.kt
@@ -6,18 +6,35 @@ import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import com.google.samples.propertyanimation.R
 import com.google.samples.propertyanimation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    lateinit var star: ImageView
+    lateinit var rotateButton: Button
+    lateinit var translateButton: Button
+    lateinit var scaleButton: Button
+    lateinit var fadeButton: Button
+    lateinit var colorizeButton: Button
+    lateinit var showerButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        binding.rotateButton.setOnClickListener { rotater() }
+
+        star = findViewById(R.id.star)
+        rotateButton = findViewById<Button>(R.id.rotateButton)
+        translateButton = findViewById<Button>(R.id.translateButton)
+        scaleButton = findViewById<Button>(R.id.scaleButton)
+        fadeButton = findViewById<Button>(R.id.fadeButton)
+        colorizeButton = findViewById<Button>(R.id.colorizeButton)
+        showerButton = findViewById<Button>(R.id.showerButton)
+
+        rotateButton.setOnClickListener { rotater() }
+        translateButton.setOnClickListener { translater() }
     }
 
     private fun ObjectAnimator.disableViewDuringAnimation(view: View) {
@@ -32,11 +49,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rotater() {
-        val animator = ObjectAnimator.ofFloat(binding.star, View.ROTATION, -360f, 0f)
+        val animator = ObjectAnimator.ofFloat(star, View.ROTATION, -360f, 0f)
         with(animator) {
             duration = 1000
-            disableViewDuringAnimation(binding.rotateButton)
+            disableViewDuringAnimation(rotateButton)
             start()
         }
+    }
+    private fun translater() {
+        val animator = ObjectAnimator.ofFloat(star, View.TRANSLATION_X, 200f)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(translateButton)
+        animator.start()
     }
 }
