e4bc514bca27bd9975126ae31601a9f5f4a8787d
Dwiko Indrawansyah <wesibrani@gmail.com>
2023-01-05 00:34:40 +0700

    Membuat agar notifikasi dapat diklik

--- app/src/main/java/org/d3if4097/mobpro2/notify/NotificationUtils.kt
+++ app/src/main/java/org/d3if4097/mobpro2/notify/NotificationUtils.kt
@@ -2,15 +2,24 @@ package org.d3if4097.mobpro2.notify
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
+import org.d3if4097.mobpro2.MainActivity
 import org.d3if4097.mobpro2.R
 
 private const val NOTIFICATION_ID = 0
 
+@RequiresApi(Build.VERSION_CODES.M)
 fun NotificationManager.sendNotification(context: Context) {
+    val intent = Intent(context, MainActivity::class.java)
+    val pendingIntent = PendingIntent.getActivity(context,
+        NOTIFICATION_ID, intent, PendingIntent.FLAG_IMMUTABLE)
+
     val builder = NotificationCompat.Builder(
         context,
         context.getString(R.string.notif_channel_id)
@@ -18,6 +27,9 @@ fun NotificationManager.sendNotification(context: Context) {
         .setSmallIcon(R.mipmap.ic_launcher_round)
         .setContentTitle(context.getString(R.string.notif_title))
         .setContentText(context.getString(R.string.notif_message))
+        .setContentIntent(pendingIntent)
+        .setAutoCancel(true)
+
     createChannel(context)
     notify(NOTIFICATION_ID, builder.build())
 }
