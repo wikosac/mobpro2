5dc727b496a8339db87d1219a5818d5482c282c4
Dwiko Indrawansyah <wesibrani@gmail.com>
2023-01-09 19:10:11 +0700

    Membuat widget dapat diklik

--- app/src/main/java/org/d3if4097/mobpro2/widget/CovidWidgetProvider.kt
+++ app/src/main/java/org/d3if4097/mobpro2/widget/CovidWidgetProvider.kt
@@ -1,11 +1,32 @@
 package org.d3if4097.mobpro2.widget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import org.d3if4097.mobpro2.MainActivity
+import org.d3if4097.mobpro2.R
 
 class CovidWidgetProvider : AppWidgetProvider() {
-    override fun onUpdate(context: Context?, manager: AppWidgetManager?,
-                          ids: IntArray?) {
+
+    companion object {
+        private fun updateAppWidget(context: Context,
+                                    manager: AppWidgetManager, id: Int) {
+            val intent = Intent(context, MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(
+                context, 0, intent, 0)
+            val views = RemoteViews(context.packageName, R.layout.widget_main)
+            views.setOnClickPendingIntent(R.id.dataTextView, pendingIntent)
+            manager.updateAppWidget(id, views)
+        }
+    }
+
+    override fun onUpdate(context: Context?, manager: AppWidgetManager?, ids: IntArray?) {
+        if (context == null || manager == null || ids == null) return
+        for (id in ids) {
+            updateAppWidget(context, manager, id)
+        }
     }
 }
