dbb037a77c539ed2b62caa4d02e6c35e93fecc01
Dwiko Indrawansyah <wesibrani@gmail.com>
2023-01-09 18:01:42 +0700

    Membuat widget dengan tampilan statis

---
+++ app/src/main/java/org/d3if4097/mobpro2/widget/CovidWidgetProvider.kt
@@ -0,0 +1,11 @@
+package org.d3if4097.mobpro2.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class CovidWidgetProvider : AppWidgetProvider() {
+    override fun onUpdate(context: Context?, manager: AppWidgetManager?,
+                          ids: IntArray?) {
+    }
+}
